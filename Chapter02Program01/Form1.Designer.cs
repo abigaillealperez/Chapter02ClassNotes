b1fb825e1da4dd062ae0d67cfeda3a4629aa8b4f
abigaillealperez <lealperezabigail.alp@gmail.com>
2020-01-30 17:35:52 -0500

    Create Form1.Designer.cs

---
+++ Chapter02Program01/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Chapter02Program01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDisplayOutput = new System.Windows.Forms.Button();
+            this.btbExit = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.lblZip = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtZip = new System.Windows.Forms.TextBox();
+            this.txtDisplayOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDisplayOutput
+            // 
+            this.btnDisplayOutput.Location = new System.Drawing.Point(11, 112);
+            this.btnDisplayOutput.Name = "btnDisplayOutput";
+            this.btnDisplayOutput.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplayOutput.TabIndex = 0;
+            this.btnDisplayOutput.Text = "Display";
+            this.btnDisplayOutput.UseVisualStyleBackColor = true;
+            this.btnDisplayOutput.Click += new System.EventHandler(this.btnDisplayOutput_Click);
+            // 
+            // btbExit
+            // 
+            this.btbExit.Location = new System.Drawing.Point(321, 112);
+            this.btbExit.Name = "btbExit";
+            this.btbExit.Size = new System.Drawing.Size(75, 23);
+            this.btbExit.TabIndex = 1;
+            this.btbExit.Text = "Exit";
+            this.btbExit.UseVisualStyleBackColor = true;
+            this.btbExit.Click += new System.EventHandler(this.btbExit_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblName.Location = new System.Drawing.Point(11, 12);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(75, 20);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name:";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAddress.Location = new System.Drawing.Point(11, 45);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(75, 20);
+            this.lblAddress.TabIndex = 3;
+            this.lblAddress.Text = "Address:";
+            this.lblAddress.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblCity
+            // 
+            this.lblCity.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCity.Location = new System.Drawing.Point(11, 78);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(75, 20);
+            this.lblCity.TabIndex = 4;
+            this.lblCity.Text = "City:";
+            this.lblCity.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblState
+            // 
+            this.lblState.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblState.Location = new System.Drawing.Point(198, 78);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(40, 20);
+            this.lblState.TabIndex = 5;
+            this.lblState.Text = "State:";
+            this.lblState.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblZip
+            // 
+            this.lblZip.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblZip.Location = new System.Drawing.Point(290, 78);
+            this.lblZip.Name = "lblZip";
+            this.lblZip.Size = new System.Drawing.Size(40, 20);
+            this.lblZip.TabIndex = 6;
+            this.lblZip.Text = "Zip:";
+            this.lblZip.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(92, 12);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(304, 20);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(92, 46);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(304, 20);
+            this.txtAddress.TabIndex = 8;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(92, 78);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 9;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(244, 78);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(40, 20);
+            this.txtState.TabIndex = 10;
+            // 
+            // txtZip
+            // 
+            this.txtZip.Location = new System.Drawing.Point(336, 79);
+            this.txtZip.Name = "txtZip";
+            this.txtZip.Size = new System.Drawing.Size(60, 20);
+            this.txtZip.TabIndex = 11;
+            // 
+            // txtDisplayOutput
+            // 
+            this.txtDisplayOutput.Location = new System.Drawing.Point(11, 141);
+            this.txtDisplayOutput.Multiline = true;
+            this.txtDisplayOutput.Name = "txtDisplayOutput";
+            this.txtDisplayOutput.ReadOnly = true;
+            this.txtDisplayOutput.Size = new System.Drawing.Size(385, 129);
+            this.txtDisplayOutput.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 286);
+            this.Controls.Add(this.txtDisplayOutput);
+            this.Controls.Add(this.txtZip);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblZip);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btbExit);
+            this.Controls.Add(this.btnDisplayOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDisplayOutput;
+        private System.Windows.Forms.Button btbExit;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label lblZip;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtZip;
+        private System.Windows.Forms.TextBox txtDisplayOutput;
+    }
+}
+
